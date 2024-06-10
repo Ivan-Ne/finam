7bf31868ac3d9f1c65b2109b39c1af354240b3e4
neverov.iv@gmail.com <neverov.iv@gmail.com>
2024-06-10 23:31:57 +0300

    .env added

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,38 @@
-import pytest
-from selene import browser
-
-
-@pytest.fixture(scope='function', autouse=True)
-def browser_personal_settings():
-    browser.config.window_width = 1300
-    browser.config.window_height = 1000
-    browser.open('https://www.finam.ru/')
-    browser.element('.p15x.window-close.pointer').click()
-    yield
-    browser.quit()
+# import pytest
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
+# from selene import browser
+# from dotenv import load_dotenv
+# import os
+#
+#
+# @pytest.fixture(scope="session", autouse=True)
+# def load_env():
+#     load_dotenv()
+#
+#
+# selenoid_login = os.getenv("SELENOID_LOGIN")
+# selenoid_pass = os.getenv("SELENOID_PASS")
+# selenoid_url = os.getenv("SELENOID_URL")
+#
+#
+# @pytest.fixture(scope='function', autouse=True)
+# def browser_personal_settings():
+#     options = Options()
+#     selenoid_capabilities = {
+#         "browserName": "chrome",
+#         "browserVersion": "100.0",
+#         "selenoid:options": {
+#             "enableVNC": True,
+#             "enableVideo": False
+#         }
+#     }
+#
+#     options.capabilities.update(selenoid_capabilities)
+#     driver = webdriver.Remote(
+#         command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
+#         options=options)
+#
+#     browser.config.driver = driver
+#     yield browser
+#     browser.quit()
--- tests/test_finam.py
+++ tests/test_finam.py
@@ -2,26 +2,28 @@ from models.pages.finam_page import finamPage
 import allure
 
 
-def test_open_investments_topic(browser_personal_settings):
+# https://jenkins.autotests.cloud/job/build_nvr_0406241/
+
+def test_open_investments_topic():
     with allure.step("Открываем раздел 'Инвестиции'"):
         finamPage.open_investments()
 
 
-def test_open_premium_topic(browser_personal_settings):
+def test_open_premium_topic():
     with allure.step("Открываем раздел 'Премиум'"):
         finamPage.open_premium()
 
 
-def test_open_education_topic(browser_personal_settings):
+def test_open_education_topic():
     with allure.step("Открываем раздел 'Обучение'"):
         finamPage.open_education()
 
 
-def test_open_loyal_program_topic(browser_personal_settings):
+def test_open_loyal_program_topic():
     with allure.step("Открываем раздел 'Программа лояльности'"):
         finamPage.open_loyal_program()
 
 
-def test_open_ipo_calendar(browser_personal_settings):
+def test_open_ipo_calendar():
     with allure.step("Открываем раздел 'Календарь -> IPO'"):
         finamPage.open_calendar_of_ipo()
